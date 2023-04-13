a58efbe0cffbadc1f8b838cb71edf7b9c7ad421c
Jlyu015 <jlyu015@github.com>
2023-04-13 00:00:29 -0700

    initial commit

---
+++ Assets/character_script.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character_script : MonoBehaviour
+{
+    public Rigidbody2D myRigidBody;
+    public float moveSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            myRigidBody.velocity = Vector2.up * 10;
+        }
+        else if(Input.GetKey(KeyCode.A)) {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+        }
+        
+
+        
+        
+        
+    }
+}
