e134cc8f8406a65be2226d2f91f90de5cbf4aa0e
Bryan Dang <bdang014@ucr.edu>
2023-04-24 14:06:04 -0700

    Direction Changes, Color Changes, Jump Height/Length scaling, avoided input glitching

--- Assets/character_script.cs
+++ Assets/character_script.cs
@@ -11,7 +11,9 @@ public class character_script : MonoBehaviour
     public float moveSpeed = 10;
     public float buttonTime = 0.5f;
     public float jumpHeight = 10;
+    public float jumpLength = 0;
     public float cancelRate = 100;
+    public float dir = 0;
     public float moveInput;
     
     private float jumpTime;
@@ -42,38 +44,41 @@ public class character_script : MonoBehaviour
         
         float horizontalInput = Input.GetAxisRaw("Horizontal"); 
 
-        if(isGrounded && !Input.GetKey(KeyCode.Space)) { // cant walk if charging jump
-            myRigidBody.velocity = new Vector2(horizontalInput * moveSpeed, myRigidBody.velocity.y);
-        }
-        
-        if(isGrounded) {
-            renderer.color = Color.blue;
-        }
-        else {
-            renderer.color = Color.red;
-        }
-        
+        if (!isGrounded){
+        } else {
+            if(isGrounded && !Input.GetKey(KeyCode.Space)) { // cant walk if charging jump
+                myRigidBody.velocity = new Vector2(horizontalInput * moveSpeed, myRigidBody.velocity.y);
+            }
+            
+            if(!isGrounded) {
+                renderer.color = Color.red;
+            }
+            
 
-        if (Input.GetKey(KeyCode.Space) && isGrounded && canJump && jumpHeight <= 15) { // charging jump
-            jumpHeight += .1f;
-        }
+            if (Input.GetKey(KeyCode.Space) && isGrounded && canJump && jumpHeight <= 12) { // charging jump
+                myRigidBody.velocity = new Vector2(0, myRigidBody.velocity.y);
+                jumpHeight += .2f;
+                renderer.color = Color.blue;
+            }
 
-        if(Input.GetKeyUp(KeyCode.Space)) { // jumping
-            canJump = true;
-            myRigidBody.velocity = new Vector2(moveInput, jumpHeight);
-            jumpHeight = 0;
-        }
+            if(Input.GetKeyUp(KeyCode.Space)) { // jumping
+                canJump = true;
+                jumpLength += horizontalInput;
+                myRigidBody.velocity = new Vector2(jumpLength, jumpHeight);
+                jumpHeight = 0;
+            }
 
 
-        if (myRigidBody.velocity.x > 0 && isGrounded) {
-            gameObject.transform.localScale = new Vector3(1, 1, 1);
-            renderer.color = Color.cyan;
-        } 
-        else if (myRigidBody.velocity.x < 0 && isGrounded) {
-            gameObject.transform.localScale = new Vector3(-1, 1, 1);
-            renderer.color = Color.magenta;
+            if (myRigidBody.velocity.x > 0 && isGrounded) {
+                gameObject.transform.localScale = new Vector3(1, 1, 1);
+                renderer.color = Color.cyan;
+            } 
+            else if (myRigidBody.velocity.x < 0 && isGrounded) {
+                gameObject.transform.localScale = new Vector3(-1, 1, 1);
+                renderer.color = Color.magenta;
+            }
+            
         }
-        
     }
 
     private void OnDrawGizmos() {
