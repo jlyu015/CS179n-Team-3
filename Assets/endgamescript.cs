4edf085e9e90e2f9bf0306cc8e45062608490811
Ryan Ly <ryanly36@gmail.com>
2023-05-14 21:18:54 -0700

    if you reach the set end point, it will display you win and then after 2 seconds return to main menu

---
+++ Assets/endgamescript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class endgamescript : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    public GameObject squareObject;
+    public TextMeshProUGUI winText;
+
+    private bool gameEnded = false;
+
+    void Update()
+    {
+        if (!gameEnded && IsPlayerInBounds())
+        {
+            gameEnded = true;
+            StartCoroutine(EndGameCoroutine());
+        }
+    }
+
+    private bool IsPlayerInBounds()
+    {
+        Bounds bounds = squareObject.GetComponent<Renderer>().bounds;
+        return bounds.Contains(playerCharacter.transform.position);
+    }
+
+    IEnumerator EndGameCoroutine()
+    {
+        // Player has left the square area, show the "YOU WIN" text
+        winText.gameObject.SetActive(true);
+        winText.text = "YOU WIN!";
+
+        yield return new WaitForSeconds(2f);
+
+        SceneManager.LoadScene("mainmenu");
+    }
+}
